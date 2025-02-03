94b4d815af59e01e15385594b241178042577cc5
justcgh9 <a.kabardiyadi@innopolis.university>
2025-02-03 21:40:03 +0300

    fix lint step

--- app_python/app/main.py
+++ app_python/app/main.py
@@ -8,13 +8,11 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="app/templates")
 
+
 @app.get("/", response_class=HTMLResponse)
 async def get_moscow_time(request: Request):
     """Displays the current Moscow time"""
     moscow_timezone = pytz.timezone("Europe/Moscow")
     current_time = datetime.now(moscow_timezone).strftime("%Y-%m-%d %H:%M:%S")
 
-    return templates.TemplateResponse(
-    request, "index.html", {"time": current_time}
-)
-
+    return templates.TemplateResponse(request, "index.html", {"time": current_time})
